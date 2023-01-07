f225db76d60d423e792aa1750caa68d8c5c8b791
raphael.dev <74559168+RaafaDev@users.noreply.github.com>
2023-01-07 23:28:44 +0100

    Delete Kartable-Bypasser directory

--- Kartable-Bypasser/background.js
+++
@@ -1,6 +0,0 @@
-// Fichier background.js
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: "removeOverlays"});
-  });
-});
